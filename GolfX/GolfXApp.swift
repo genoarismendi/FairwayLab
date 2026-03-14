dea91ec941d3f887bdd96d283e30b87b24ac202d
g <genaroarismendi@icloud.com>
2026-03-14 13:07:21 +0000

    Initial Commit

---
+++ GolfX/GolfXApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfXApp.swift
+//  GolfX
+//
+//  Created by g on 14-03-26.
+//
+
+import SwiftUI
+
+@main
+struct GolfXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GolfXTests/GolfXTests.swift
@@ -0,0 +1,17 @@
+//
+//  GolfXTests.swift
+//  GolfXTests
+//
+//  Created by g on 14-03-26.
+//
+
+import Testing
+@testable import GolfX
+
+struct GolfXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
